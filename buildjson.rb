1b3f3ded3c3b7c422f7eb571505421649c3dccae
Matthijs van der Vleuten <git@zr40.nl>
2013-11-26 20:26:00 +0100

    split apps.yaml

--- buildjson.rb
+++ buildjson.rb
@@ -3,8 +3,17 @@
 require 'yaml'
 require 'json'
 
-data = YAML.load_file 'apps.yaml'
-data['release'] = Time.now.to_i
+data = {
+	'version' => 1,
+	'release' => Time.now.to_i,
+	'categories' => YAML.load_file('src/categories.yaml'),
+	'engines' => YAML.load_file('src/engines.yaml'),
+	'apps' => YAML.load_file('src/apps.yaml'),
+}
+
+Dir['src/apps/*.yaml'].each do |app_filename|
+	data['apps'][File.basename app_filename, '.yaml'] = YAML.load_file app_filename
+end
 
 File.open 'apps.json', 'w' do |file|
  file.write data.to_json :array_nl => "\n", :object_nl => "\n", :indent => "\t"
